5b0a907f1fb1c9f2209b7ff2253c17ed22a86ce3
saske-pro <46398494+saske-pro@users.noreply.github.com>
2019-01-14 12:36:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**هلووافريري  وون @everyone  **[ " ${x} " ]`)
+        message.channel.send(`**هلووافريري  وون   **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
